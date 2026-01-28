7c35e089df15d92d82a2a7b96d2128537fc969f5
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-01-28 16:50:28 +0100

    fix style product card

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,52 +1,57 @@
 function ProductCard({ product }) {
 
-  const { title, price, description, category, image } = product;
+    const { title, price, description, category, image, rating } = product;
 
-  return (
-    <div className="lg-4">
-      <div className="card h-100 shadow-sm">
+    return (
+        <div className="lg-4">
+            <div className="card h-100 shadow-sm">
 
-        <div className="card-body d-flex flex-column">
+                <div className="card-body d-flex flex-column">
 
-          {/* Titolo*/}
-          <h5 className="card-title fw-bold mb-2 text-center">
-            {title}
-          </h5>
+                    {/* Titolo*/}
+                    <h5 className="card-title fw-bold mb-2 text-center">
+                        {title}
+                    </h5>
 
-          {/* Immagine */}
-          <div className="d-flex align-items-center justify-content-center p-3">
-            <img
-              src={image}
-              alt={title}
-              className="img-fluid"
-              style={{
-                maxHeight: "260px",
-                objectFit: "contain"
-              }}
-            />
-          </div>
+                    {/* Immagine */}
+                    <div className="d-flex align-items-center justify-content-center p-3">
+                        <img
+                            src={image}
+                            alt={title}
+                            className="img-fluid"
+                            style={{
+                                maxHeight: "260px",
+                                objectFit: "contain"
+                            }}
+                        />
+                    </div>
 
-          {/* Categoria */}
-          <span className="badge bg-secondary align-self-center mb-3">
-            {category}
-          </span>
+                    {/* Categoria */}
+                    <span className="badge bg-secondary align-self-center mb-3">
+                        {category}
+                    </span>
 
-          {/* Descrizione */}
-          <p className="small text-center text-muted mb-3">
-            {description}
-          </p>
+                    {/* Descrizione */}
+                    <p className="small text-center text-muted mb-3">
+                        {description}
+                    </p>
 
-          {/* Prezzo */}
-          <div className="d-flex justify-content-end mt-auto">
-            <span className="text-primary fs-5 fw-bold">
-              € {price}
-            </span>
-          </div>
+                    {/* Rating */}
+                    <p className="small text-center  mb-3">
+                        ⭐ {rating.rate} / 5 ({rating.count} recensioni)
+                    </p>
 
+                    {/* Prezzo */}
+                    <p className="text-primary text-center fs-5 fw-bold">
+                        € {price}
+                    </p>
+
+
+
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ProductCard;
