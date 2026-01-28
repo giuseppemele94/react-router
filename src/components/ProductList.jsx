1c89a1274e2d0e52160e3174ba74436a48cc8f16
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-01-28 17:14:43 +0100

    fix pagina prodotti gestisce la chiamata API

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,33 +2,18 @@ import { useState, useEffect } from "react"
 import ProductCard from "./productCard"
 import axios from "axios";
 
-function ProductList() {
+function ProductList({products}) {
 
-    //var di stato che conterrà la lista dei prodotti 
-    const [products, setProducts] = useState([]);
-    const endpoint = "https://fakestoreapi.com/products";
-    //montaggio del componente al cambio di Route 
-    useEffect(() => {
-
-        axios.get(endpoint)
-            .then((res) => {
-                setProducts(res.data);
-
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }, []); //viene eseguito una sola volta quando entro nella pagina 
+    
 
     return (
         <div className="container-md py-5">
             <h1 className="mb-4 d-flex align-items-center justify-content-center p-3">Prodotti</h1>
-
             <div className="row g-4 justify-content-center">
                 {products.map(product => (
-                    <ProductCard 
-                    key={product.id} 
-                    product={product} />
+                    <ProductCard
+                        key={product.id}
+                        product={product} />
                 ))}
             </div>
         </div>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,14 +1,31 @@
-
+import axios from "axios";
 import ProductList from "../components/ProductList"
+import { useEffect, useState } from "react";
 
 function Products() {
-  
 
-  return (
-    <>
-      <ProductList/>
-    </>
-  )
+    //var di stato che conterrà la lista dei prodotti 
+    const [products, setProducts] = useState([]);
+    const endpoint = "https://fakestoreapi.com/products";
+
+    //montaggio del componente al cambio di Route 
+    useEffect(() => {
+
+        axios.get(endpoint)
+            .then((res) => {
+                setProducts(res.data);
+
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, []); //viene eseguito una sola volta quando entro nella pagina 
+
+    return (
+       
+            <ProductList products={products} />
+        
+    )
 }
 
 export default Products
