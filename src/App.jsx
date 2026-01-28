8cce2b0e7ab8d5a9e203fd1824d95c17b0e70791
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-01-28 15:20:16 +0100

    creata rotta di navigazione

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import HomePage from "./pages/HomePage"
+import AboutUs from "./pages/AboutUs"
+import Products from "./pages/Products"
+import DefaultLayout from "./layouts/DefaultLayout"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 
 function App() {
-  
+
 
   return (
-    <>
-      <h1>Ciao</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chisiamo" element={<AboutUs />} />
+          <Route path="/prodotti" element={<Products />} />
+        </Route>
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
