52d6c855ce0d1eb77ca3a56bd71d183291fcf564
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-01-29 15:10:44 +0100

    useParams con id per collegamento pagina singola del prodotto con riutilizzo cardProduct per stampare la singola card

--- src/pages/ProductsDetail.jsx
+++ src/pages/ProductsDetail.jsx
@@ -1,13 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import ProductCard from "../components/productCard";
 
+
+const endpointBase = "https://fakestoreapi.com/products/";
 
 function ProductsDetail() {
 
-    
+     // recuper valore param dinamico grazie a hook useParams
+    const { id } = useParams();
+
+    // var di stato per salvare info oggetto prodotto
+    const [product, setProduct] = useState();
    
+     // utilizzo il parametro per la chiamata corretta (sempre passando da hook su primo montaggio)
+    useEffect(() => {
+        axios.get(endpointBase + id)
+            .then(resp => setProduct(resp.data))
+            .catch(err => {
+                console.log("errore sulla chiamata", err)
+            })
+    }, [])
+
     return (
        
-            <ProductList products={products} />
-        
+        <>
+            <h1 className="mb-4 d-flex align-items-center justify-content-center p-3">Dettaglio del Prodotto</h1>
+            {product ? (
+                <ProductCard product={product} />
+            ) : (
+                <p className="loader">Loading...</p>
+            )
+            }
+
+        </>
     )
 }
 
